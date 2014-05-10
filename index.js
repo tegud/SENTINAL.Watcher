33bf0dee1a568abf81cc0c5991c13ac35f033f2e
Steve Elliott <steve@tegud.net>
2014-05-10 09:50:03 +0100

    Split index logic for co-ordinating config load
    Move into config/index, less giant block in index.js

--- index.js
+++ index.js
@@ -1,37 +1,18 @@
 var async = require('async');
 var _ = require('lodash');
-var alerts = require('./lib/alerts');
-var configLoader = require('./lib/config/loader');
-
-var dir = __dirname + '/config';
+var config = require('./lib/config');
 
 async.waterfall([
-		async.apply(configLoader.loadConfigurationForModules, dir, ['alerts', 'notifiers', 'sources']),
-		function(results, callback) {
-			var configuredAlerts = results[0];
-			var configuredNofifiers = results[1];
-			var configuredSources = results[2];
-
-			var alertsInConfig = _.reduce(alerts, function(memo, alert, alertName) {
-				if(_.contains(Object.keys(configuredAlerts), alertName)) {
-					memo[alertName] = alert;
-				}
-				return memo;
-			}, {});
-
-			var alertConfigTasks = _.map(alertsInConfig, function(alert, alertName) {
-				return async.apply(alert.configure, configuredAlerts[alertName]);
+		async.apply(config.configureModules, __dirname + '/config'), 
+		function (results, callback) {
+			var alertCheckTasks = _.map(results[0].config, function(alert, alertName) {
+				return require('./lib/' + results[0].name + '/' + alertName).check;
 			});
 
-			var alertCheckTasks = _.pluck(alertsInConfig, 'check');
-
-			console.log('Configuring ' + alertConfigTasks.length + ' alert(s)...');
-
-			async.parallel(alertConfigTasks, function(err) {
-				callback(err, alertCheckTasks);
-			});
+			callback(null, alertCheckTasks);
 		}
-	], function(err, alertCheckTasks) {
+	], 
+	function(err, alertCheckTasks) {
 		var ping = function() {
 			console.log('Checking alerts...');
 
