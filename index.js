7e818b52585cdd588fbff1f51d091ee85d8dbd12
Steve Elliott <steve@tegud.net>
2014-05-10 22:07:39 +0100

    Index functionality split apart
    Restructured code to better separate the logic.  Alerts will now be able
    to be individually scheduled (elasticsearch-lag defaults to 10s). All
    modules support initialisation and configuration.

--- index.js
+++ index.js
@@ -1,26 +1,12 @@
 var async = require('async');
 var _ = require('lodash');
 var config = require('./lib/config');
+var modules = require('./lib/modules');
 
-async.waterfall([
+async.series([
 		async.apply(config.configureModules, __dirname + '/config'), 
-		function (results, callback) {
-			var alertCheckTasks = _.map(results[0].config, function(alert, alertName) {
-				return require('./lib/' + results[0].name + '/' + alertName).check;
-			});
-
-			callback(null, alertCheckTasks);
-		}
+		modules.initialiseAll
 	], 
-	function(err, alertCheckTasks) {
-		var ping = function() {
-			console.log('Checking alerts...');
-
-			async.parallel(alertCheckTasks, function() {
-				console.log('alert checks complete');
-				setTimeout(ping, 5000);
-			});
-		};
-
-		ping();
-});
+	function(err) {
+		console.log('Start up of SENTINAL.Watcher completed.');
+	});
