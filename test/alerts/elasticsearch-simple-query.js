eb6cb2697e78a0e561b2e3104dcf822d4b6d57fe
Steve Elliott <steve@tegud.net>
2014-05-14 21:09:55 +0100

    Test to drive threshold functionality

--- test/alerts/elasticsearch-simple-query.js
+++ test/alerts/elasticsearch-simple-query.js
@@ -7,135 +7,149 @@ var moment = require('moment');
 
 var currentDate = '01-01-2014 00:00 Z';
 
+var fakeScheduler = {
+	createFromConfig: function(config, task) {
+		return {
+			start: function() {
+				task();
+			},
+			stop: function() {},
+			scheduleNext: function() {}
+		};
+	}
+};
+
 describe('elasticsearch-errors', function() {
-	var elasticsearchSimpleQueryAlert;
-	var actualRequest;
+	describe('queries elasticsearch', function() {
+		var elasticsearchSimpleQueryAlert;
+		var actualRequest;
+		var currentDate = '01-01-2014 00:00 Z';
 
-	beforeEach(function() {
-		actualRequest = null;
-		elasticsearchSimpleQueryAlert = proxyquire('../../lib/modules/alerts/elasticsearch-simple-query', {
-			'../../modules/schedulers': {
-				createFromConfig: function(config, task) {
-					return {
-						start: function() {
-							task();
-						},
-						stop: function() {},
-						scheduleNext: function() {}
-					};
-				}
-			},
-			'elasticsearch': {
-				Client: function() {
-					return {
-						search: function(request) {
-							actualRequest = request;
+		beforeEach(function() {
+			actualRequest = null;
+			elasticsearchSimpleQueryAlert = proxyquire('../../lib/modules/alerts/elasticsearch-simple-query', {
+				'../../modules/schedulers': fakeScheduler,
+				'elasticsearch': {
+					Client: function() {
+						return {
+							search: function(request) {
+								actualRequest = request;
 
-							return {
-								then: function(callback) {
-									callback({
-										hits: {
-											hits: []
-										}
-									});
-								}
-							};
-						}
-					};
+								return {
+									then: function(callback) {
+										callback({
+											hits: {
+												hits: []
+											}
+										});
+									}
+								};
+							}
+						};
+					}
+				},
+				'moment': function() {
+					return moment(currentDate, 'DD-MM-YYYY HH:mm Z');
 				}
-			},
-			'moment': function() {
-				return moment(currentDate, 'DD-MM-YYYY HH:mm Z');
-			}
+			});
+		});
+
+		it('queries today and yesterday\'s logstash indicies', function(done){
+			currentDate = '14-05-2014 00:00 Z';
+			var expectedIndex = 'logstash-2014.05.14,logstash-2014.05.13';
+
+			async.series([
+				async.apply(elasticsearchSimpleQueryAlert.configure, { }),
+				elasticsearchSimpleQueryAlert.initialise,
+				function(callback) {
+					expect(actualRequest.index).to.be(expectedIndex);
+					callback();
+				}
+			], 
+			done);
+		});
+
+		it('queries elasticsearch with the configured query', function(done) {
+			async.series([
+				async.apply(elasticsearchSimpleQueryAlert.configure, {
+					query: 'keyword'
+				}),
+				elasticsearchSimpleQueryAlert.initialise,
+				function(callback) {
+					expect(actualRequest.body.query.filtered.query.bool.should[0]['query_string']['query']).to.be('keyword');
+					callback();
+				}
+			], 
+			done);
+		});
+
+		it('filters elasticsearch query with a timestamp range ending at the current date and time', function(done) {
+			currentDate = '14-05-2014 16:23 Z';
+
+			async.series([
+				async.apply(elasticsearchSimpleQueryAlert.configure, {
+					time: '10 minutes'
+				}),
+				elasticsearchSimpleQueryAlert.initialise,
+				function(callback) {
+					expect(actualRequest.body.query.filtered.filter.bool.must[0].range['@timestamp'].to).to.be(moment(currentDate, 'DD-MM-YYYY HH:mm Z').valueOf());
+					callback();
+				}
+			], 
+			done);
+		});
+
+		it('filters elasticsearch query with a timestamp range starting at the current date and time', function(done) {
+			currentDate = '14-05-2014 16:23 Z';
+			tenMinutesBefore = moment(currentDate, 'DD-MM-YYYY HH:mm Z').subtract('minutes', 10);
+
+			async.series([
+				async.apply(elasticsearchSimpleQueryAlert.configure, {
+					time: '10 minutes'
+				}),
+				elasticsearchSimpleQueryAlert.initialise,
+				function(callback) {
+					expect(actualRequest.body.query.filtered.filter.bool.must[0].range['@timestamp'].from).to.be(tenMinutesBefore.valueOf());
+					callback();
+				}
+			], 
+			done);
+		});
+
+		it('does not filter elasticsearch query when no time specified', function(done) {
+			async.series([
+				async.apply(elasticsearchSimpleQueryAlert.configure, { }),
+				elasticsearchSimpleQueryAlert.initialise,
+				function(callback) {
+					expect(actualRequest.body.query.filtered.filter).to.be(undefined);
+					callback();
+				}
+			], 
+			done);
+		});
+
+		it('limits number of results that the elasticsearch query returns to the configured value', function(done) {
+			async.series([
+				async.apply(elasticsearchSimpleQueryAlert.configure, {
+					limitResultsTo: 100
+				}),
+				elasticsearchSimpleQueryAlert.initialise,
+				function(callback) {
+					expect(actualRequest.body.size).to.be(100);
+					callback();
+				}
+			], 
+			done);
 		});
 	});
 
-	it('queries today and yesterday\'s logstash indicies', function(done){
-		currentDate = '14-05-2014 00:00 Z';
-		var expectedIndex = 'logstash-2014.05.14,logstash-2014.05.13';
-
-		async.series([
-			async.apply(elasticsearchSimpleQueryAlert.configure, { }),
-			elasticsearchSimpleQueryAlert.initialise,
-			function(callback) {
-				expect(actualRequest.index).to.be(expectedIndex);
-				callback();
-			}
-		], 
-		done);
-	});
-
-	it('queries elasticsearch with the configured query', function(done) {
-		async.series([
-			async.apply(elasticsearchSimpleQueryAlert.configure, {
-				query: 'keyword'
-			}),
-			elasticsearchSimpleQueryAlert.initialise,
-			function(callback) {
-				expect(actualRequest.body.query.filtered.query.bool.should[0]['query_string']['query']).to.be('keyword');
-				callback();
-			}
-		], 
-		done);
-	});
-
-	it('filters elasticsearch query with a timestamp range ending at the current date and time', function(done) {
-		currentDate = '14-05-2014 16:23 Z';
-	
-		async.series([
-			async.apply(elasticsearchSimpleQueryAlert.configure, {
-				time: '10 minutes'
-			}),
-			elasticsearchSimpleQueryAlert.initialise,
-			function(callback) {
-				expect(actualRequest.body.query.filtered.filter.bool.must[0].range['@timestamp'].to).to.be(moment(currentDate, 'DD-MM-YYYY HH:mm Z').valueOf());
-				callback();
-			}
-		], 
-		done);
-	});
-
-	it('filters elasticsearch query with a timestamp range starting at the current date and time', function(done) {
-		currentDate = '14-05-2014 16:23 Z';
-		tenMinutesBefore = moment(currentDate, 'DD-MM-YYYY HH:mm Z').subtract('minutes', 10);
-	
-		async.series([
-			async.apply(elasticsearchSimpleQueryAlert.configure, {
-				time: '10 minutes'
-			}),
-			elasticsearchSimpleQueryAlert.initialise,
-			function(callback) {
-				expect(actualRequest.body.query.filtered.filter.bool.must[0].range['@timestamp'].from).to.be(tenMinutesBefore.valueOf());
-				callback();
-			}
-		], 
-		done);
-	});
-
-	it('does not filter elasticsearch query when no time specified', function(done) {
-		async.series([
-			async.apply(elasticsearchSimpleQueryAlert.configure, { }),
-			elasticsearchSimpleQueryAlert.initialise,
-			function(callback) {
-				expect(actualRequest.body.query.filtered.filter).to.be(undefined);
-				callback();
-			}
-		], 
-		done);
-	});
-
-	it('limits number of results that the elasticsearch query returns to the configured value', function(done) {
-		async.series([
-			async.apply(elasticsearchSimpleQueryAlert.configure, {
-				limitResultsTo: 100
-			}),
-			elasticsearchSimpleQueryAlert.initialise,
-			function(callback) {
-				expect(actualRequest.body.size).to.be(100);
-				callback();
-			}
-		], 
-		done);
+	describe('handles the response from elasticsearch', function() {
+		beforeEach(function() {
+			actualRequest = null;
+			elasticsearchSimpleQueryAlert = proxyquire('../../lib/modules/alerts/elasticsearch-simple-query', {
+				'../../modules/schedulers': fakeScheduler
+			});
+		});
 	});
 });
 		// nock('http://myelasticsearch.com:9200')
