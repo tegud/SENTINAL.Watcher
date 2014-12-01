db156a910d7737ed4dd98216c8b0296f114fa899
Andy Lowry <andrew.lowry@laterooms.com>
2014-12-01 17:48:56 +0000

    Add minValue Threshold

---
+++ test/modules/thresholds/minValue.js
@@ -0,0 +1,80 @@
+var expect = require('expect.js');
+
+var minValue = require('../../../lib/modules/thresholds/minValue');
+
+describe('minValue', function() {
+	it('sets expected threshold', function() {
+		var thresholdResult = new minValue({ getLastResult: function() {} }, { limit: 1 }).checkValue();
+
+		expect(thresholdResult.minValue).to.be(1);
+	});
+
+	it('sets configured level', function() {
+		var thresholdResult = new minValue({ getLastResult: function() {} }, { level: 'critical', limit: 1 }).checkValue();
+
+		expect(thresholdResult.level).to.be('critical');
+	});
+
+	it('defaults level to breach', function() {
+		var thresholdResult = new minValue({ getLastResult: function() {} }, { limit: 1 }).checkValue();
+
+		expect(thresholdResult.level).to.be('breach');
+	});
+
+	it('sets threshold description', function() {
+		var thresholdResult = new minValue({ getLastResult: function() {} }, { limit: 1 }).checkValue();
+
+		expect(thresholdResult.threshold).to.be('value < 1');
+	});
+
+	it('sets breached false when limit is less than last result', function() {
+		var thresholdResult = new minValue({ getLastResult: function() { return 2; } }, { limit: 1 }).checkValue();
+
+		expect(thresholdResult.breached).to.be(false);
+	});
+
+	it('sets breached false when limit is equal to the last result', function() {
+		var thresholdResult = new minValue({ getLastResult: function() { return 1; } }, { limit: 1 }).checkValue();
+
+		expect(thresholdResult.breached).to.be(false);
+	});
+
+	it('sets breached true when limit is greater than the last result', function() {
+		var thresholdResult = new minValue({ getLastResult: function() { return 1; } }, { limit: 2 }).checkValue();
+
+		expect(thresholdResult.breached).to.be(true);
+	});
+
+	it('checks specified property for breach', function() {
+		var thresholdResult = new minValue({ 
+			getLastResult: function() { return { myField: 2 }; } }, 
+			{ 
+				limit: 3, 
+				field: 'myField' 
+			}).checkValue();
+
+		expect(thresholdResult.breached).to.be(true);
+	});
+
+	it('sets threshold description\'s field name', function() {
+		var thresholdResult = new minValue({ getLastResult: function() { return { myField: 1 }; } }, 
+			{ 
+				limit: 1, 
+				field: 'myField' 
+			}).checkValue();
+
+		expect(thresholdResult.threshold).to.be('myField < 1');
+	});
+
+	it('ignores non numeric values', function() {
+		var thresholdResult = new minValue({ getLastResult: function() { return { myField: 'exception' }; } }, 
+			{ 
+				limit: 1, 
+				field: 'myField' 
+			}).checkValue();
+
+		console.log(thresholdResult);
+
+		expect(thresholdResult.breached).to.be(false);
+	});
+});
